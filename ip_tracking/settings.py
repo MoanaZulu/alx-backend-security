3055bb38d7d1f3ddb2d64256bc1f56c7ce3d128c
Moana <missn.hadebe@gmail.com>
2026-01-18 13:31:09 +0200

    Implement IP tracking middleware and Requesting model

--- ip_tracking/settings.py
+++ ip_tracking/settings.py
@@ -1,4 +1,21 @@
 INSTALLED_APPS = [
+    # existing apps...
+    'ip_tracking',
+]
+
+
+
+MIDDLEWARE = [
+    # existing middleware...
+    'ip_tracking.middleware.IPTrackingMiddleware',
+]
+
+
+
+
+
+
+INSTALLED_APPS = [
     ...,
     'ip_tracking',
 ]
